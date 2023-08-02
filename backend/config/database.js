8089949556d0f65c375eab550d2be038ed3c30f9
Pankaj <89864509+0410Pankajv@users.noreply.github.com>
2023-08-03 00:15:23 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDatabase = ( ) => {
 mongoose.set('strictQuery', false);
-    mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true })
+    mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
         .then( (data) => {        
             console.log(`MongoDB connected with server: ${data.connection.host}`);
         
