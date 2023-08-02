394b301984d764ebff390fa336043779cc45526e
Pankaj <89864509+0410Pankajv@users.noreply.github.com>
2023-08-03 00:02:03 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connectDatabase = ( ) => {
-
+mongoose.set('strictQuery', false);
     mongoose.connect(process.env.DB_URI)
         .then( (data) => {        
             console.log(`MongoDB connected with server: ${data.connection.host}`);
