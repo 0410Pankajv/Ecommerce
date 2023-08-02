98164b3b6ad447e6d5bef8388ff927ed8af9c01b
Pankaj <89864509+0410Pankajv@users.noreply.github.com>
2023-08-03 00:10:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ process.on("uncaughtException" , (err) => {
 // All dot.env file should call after CONFIG SETUP
 if(process.env.NODE_ENV!=="PRODUCTION"){
 
-    require("dotenv").config({path: "backend/config/config.env"});
+    require('dotenv').config({path: "backend/config/config.env"});
 }
 
 
