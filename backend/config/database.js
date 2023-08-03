f8088dd0c6fff544f43404ef91d170f64ff4012c
Pankaj <89864509+0410Pankajv@users.noreply.github.com>
2023-08-03 12:36:31 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDatabase = ( ) => {
 mongoose.set('strictQuery', false);
-    mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect("mongodb+srv://admin-pankaj:vijaypankaj4@cluster0.zk3ae.mongodb.net/Ecommerce?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
         .then( (data) => {        
             console.log(`MongoDB connected with server: ${data.connection.host}`);
         
